6df98d9c50f213142fc5c3de89d8293b99c9a75e
jackson3791 <1032399389@qq.com>
2021-01-04 16:25:07 +0800

    login

---
+++ js/bannerfade.js
@@ -0,0 +1,28 @@
+
+        var mySwiper = new Swiper('.swiper-container', {
+
+            loop: true, // 循环模式选项
+            autoplay: {
+                delay: 5000,//1秒切换一次
+              },
+            effect : 'fade',
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+                clickable:true
+            },
+
+            // 如果需要前进后退按钮
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            
+        }) 
+
+        //此方法为模拟的，hover到分页器的小圆点后自动触发其本身的click事件
+        $(".swiper-pagination-bullet").hover(function() {
+            $(this).click(); //鼠标划上去之后，自动触发点击事件来模仿鼠标划上去的事件
+        },function() {
+            mySwiper.autoplay.start(); //鼠标移出之后，自动轮播开启
+        })
