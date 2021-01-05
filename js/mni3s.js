9a4b99bf5d6be1c414a2ce23e62a29f2ea9bbf1d
jackson3791 <1032399389@qq.com>
2021-01-05 10:22:30 +0800

    2021.1.5

--- js/mni3s.js
+++ js/mni3s.js
@@ -1,14 +1,29 @@
-var time = setInterval(function () {
-    t();}, 2000)
-
-function t() {
-    var he = $(".mni3s_ul>li").height();//找到li高
-    $(".mni3s_ul>li").eq(0).appendTo($(".mni3s_ul")); //复制第一个到最后一个
-    $(".mni3s_ul").animate({
-        "marginTop": "-" + he
-    }, 500, function () {
-        $(".mni3s_ul").css({
-            "marginTop": 0
-        })
-    })
-}
+var ann_ul = $('.mni3s').find('ul');
+    // console.log(ann_ul);
+    ann_ul.index = 0;
+    ann_ul[0].timer = setInterval(function(){
+        ann_ul.index++;
+        if(ann_ul.index == ann_ul.children().length){
+            ann_ul.index = 1;
+            ann_ul.css('marginTop',0);
+        }
+        ann_ul.animate({
+            marginTop:-43 * ann_ul.index
+        })   
+    },2000)
+    ann_ul.mouseover(function(){
+        clearInterval(ann_ul[0].timer);
+        ann_ul[0].timer = null;
+    })
+    ann_ul.mouseout(function(){
+        ann_ul[0].timer = setInterval(function(){
+            ann_ul.index++;
+            if(ann_ul.index == ann_ul.children().length){
+                ann_ul.index = 1;
+                ann_ul.css('marginTop',0);
+            }
+            ann_ul.animate({
+                marginTop:-43 * ann_ul.index
+            })   
+        },2000)
+    })
