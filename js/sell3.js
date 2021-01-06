5a80a60e62b8e135e0cb0d0766e08f8cc3842442
jackson3791 <1032399389@qq.com>
2021-01-06 13:59:37 +0800

    2021-1-6 14点

--- js/sell3.js
+++ js/sell3.js
@@ -2,7 +2,7 @@ var  sell3_li = $('.sell3_box>ul').find('li');
      sell3_ul = $('.sell3_box').find('ul');
     width = $('.sell3_box>ul').width()
     sell3_ul.index = 0
-    console.log(sell3_li.length)
+    // console.log(sell3_li.length)
 
         $('.btn-prev').hide();
         $('.sell3_box>.btn-next').click(function(){
@@ -11,8 +11,9 @@ var  sell3_li = $('.sell3_box>ul').find('li');
             $(".sell3_box>ul").animate({
                 "marginLeft": -(sell3_ul.index*width)
             },500)
+            // console.log(sell3_ul.index)
 
-        if(sell3_ul.index*10 == sell3_li.length){
+        if((sell3_ul.index+1)*5 == sell3_li.length){
         $('.btn-next').hide();
         $('.btn-prev').show();
         };
@@ -21,10 +22,20 @@ var  sell3_li = $('.sell3_box>ul').find('li');
         $('.sell3_box>.btn-prev').click(function(){
             sell3_ul.index--;
             $(".sell3_box>ul").animate({
-                "marginLeft": (sell3_ul.index*width)
+                "marginLeft": -(sell3_ul.index*width)
             },500)
+            // console.log(sell3_ul.index*width)
             if(sell3_ul.index == 0){
                 $('.btn-next').show();
                 $('.btn-prev').hide();
             };
         })
+
+ 
+
+        
+       
+     
+
+        
+                
